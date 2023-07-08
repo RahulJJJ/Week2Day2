3d162c3579e92cf8cf4ec0bb10a75de41f27c97c
RahulJJJ <139000541+RahulJJJ@users.noreply.github.com>
2023-07-08 22:33:17 +0530

    Create CreateContact.java

---
+++ CreateContact.java
@@ -0,0 +1,29 @@
+package week2day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ContactTab {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("http://www.leaftaps.com/opentaps");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("demosalesManager");
+		driver.findElement(By.name("PASSWORD")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Contacts")).click();
+		driver.findElement(By.linkText("Create Contact")).click();
+		driver.findElement(By.id("firstNameField")).sendKeys("dhanasekaran");
+		driver.findElement(By.id("lastNameField")).sendKeys("venkatesan");
+		driver.findElement(By.className("smallSubmit")).click();
+		String A= driver.findElement(By.id("viewContact_firstName_sp")).getText();
+		System.out.println(A);
+		String title = driver.getTitle();
+		System.out.println(title);
+	}
+
+}
